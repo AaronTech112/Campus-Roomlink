4cf08d8e982055cd43fcaaca70d8afda6893909c
Aarontech112 <abdullahiaaron112@gmail.com>
2025-12-21 01:09:41 +0100

    signup and login design

--- core/forms.py
+++ core/forms.py
@@ -17,11 +17,22 @@ class CustomUserChangeForm(UserChangeForm):
 
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
     class Meta:
         model = User
         fields = ['full_name', 'email', 'phone_number', 'department', 'matric_number', 'password']
     
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match")
+
+        return cleaned_data
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password"])
